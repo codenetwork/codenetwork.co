4f24db140345346be3c2aaf91d467e86ad217861
Corey McCormick <coreymccor@live.com.au>
2025-04-19 16:33:14 +1000

    Add missing deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [icon()]
 });
